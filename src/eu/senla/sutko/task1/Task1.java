862d38c774bcfa594b6d39c7f54c8ff54d05df52
SUTIK <SUTIK90@MAIL.RU>
2021-01-19 09:13:28 +0300

    добавил переменные и обертки

--- src/eu/senla/sutko/task1/Task1.java
+++ src/eu/senla/sutko/task1/Task1.java
@@ -1,9 +1,27 @@
 package eu.senla.sutko.task1;
 
 public class Task1 {
+//примитивы
+    public byte a1=12;
+    public short b1=15;
+    public int c1=17;
+    public long d1=199999999999999999L;
+    public float e1=127;
+    public double f1=127.15;
+    public boolean g1=true;
+    public char h1='$';
+
+//обертки
+    Byte a2=a1;
+    Short b2;
+    Integer c2;
+    Long d2;
+    Float e2;
+    Double f2;
+    Boolean g2;
+    Character h2;
+
     public static void main(String[] args) {
-
-
 
 
     }
--- src/eu/senla/sutko/task1/varibals.java
+++ src/eu/senla/sutko/task1/varibals.java
@@ -2,87 +2,40 @@ package eu.senla.sutko.task1;
 
 public class varibals {
 
-    public void setA(byte a) {
-        this.a = a;
-    }
-
-    public void setB(short b) {
-        this.b = b;
-    }
-
-    public void setC(int c) {
-        this.c = c;
-    }
-
-    public void setD(long d) {
-        this.d = d;
-    }
-
-    public void setE(float e) {
-        this.e = e;
-    }
-
-    public void setF(double f) {
-        this.f = f;
-    }
-
-    public void setG(boolean g) {
-        this.g = g;
-    }
-
-    public void setH(char h) {
-        this.h = h;
-    }
-
-    public void setI(String i) {
-        this.i = i;
-    }
-
-    public byte getA() {
-        return a;
-    }
-
-    public short getB() {
-        return b;
-    }
-
-    public int getC() {
-        return c;
-    }
-
-    public long getD() {
-        return d;
-    }
-
-    public float getE() {
-        return e;
-    }
-
-    public double getF() {
-        return f;
-    }
-
-    public boolean isG() {
-        return g;
-    }
-
-    public char getH() {
-        return h;
-    }
-
-    public String getI() {
-        return i;
-    }
-
     public   byte a;
-    public  short b;
-    public    int c;
-    public    long d;
-    public    float e;
-    public    double f;
-    public    boolean g;
-    public    char h;
-    public    String i;
+    public byte getA() { return a; }
+    public void setA(byte a) { this.a = a; }
 
+    public  short b;
+    public short getB() {return b;}
+    public void setB(short b) {this.b = b;}
+
+    public    int c;
+    public int getC() {return c;}
+    public void setC(int c) {this.c = c;}
+
+    public    long d;
+    public long getD() {return d;}
+    public void setD(long d) {this.d = d;}
+
+    public    float e;
+    public float getE() {return e;}
+    public void setE(float e) {this.e = e;}
+
+    public    double f;
+    public double getF() {return f;}
+    public void setF(double f) {this.f = f;}
+
+    public    boolean g;
+    public boolean isG() {return g;}
+    public void setG(boolean g) {this.g = g;}
+
+    public    char h;
+    public char getH() {return h;}
+    public void setH(char h) {this.h = h;}
+
+    public    String i;
+    public String getI() {return i;}
+    public void setI(String i) {this.i = i;}
 
 }
