90738d5ddc84c275deda47af570a9ca406890216
SUTIK <SUTIK90@MAIL.RU>
2021-01-19 11:00:31 +0300

    ДЗ_1

--- src/eu/senla/sutko/task1/Task1.java
+++ src/eu/senla/sutko/task1/Task1.java
@@ -2,27 +2,110 @@ package eu.senla.sutko.task1;
 
 public class Task1 {
 //примитивы
-    public byte a1=12;
-    public short b1=15;
-    public int c1=17;
-    public long d1=199999999999999999L;
-    public float e1=127;
-    public double f1=127.15;
-    public boolean g1=true;
-    public char h1='$';
+    static public byte a1=12;
+    static public short b1=15;
+    static public int c1=17;
+    static public long d1=199999999999999999L;
+    static public float e1=127;
+    static  public double f1=127.15;
+    static public boolean g1=true;
+    static public char h1='$';
 
-//обертки
-    Byte a2=a1;
-    Short b2;
-    Integer c2;
-    Long d2;
-    Float e2;
-    Double f2;
-    Boolean g2;
-    Character h2;
 
+    //обертки с методами get и set
+    static private Byte a2;
+    public static Byte getA2() {
+
+        return a2;
+    }
+    public static void setA2(Byte a2) {
+
+        Task1.a2 = a2;
+    }
+
+
+
+    static private Short b2;
+    public static Short getB2() {
+        return b2;
+    }
+    public static void setB2(Short b2) {
+        Task1.b2 = b2;
+    }
+
+
+    static private Integer c2;
+    public static Integer getC2() {
+        return c2;
+    }
+
+    public static void setC2(Integer c2) {
+        Task1.c2 = c2;
+    }
+
+
+    static private Long d2;
+    public static Long getD2() {
+        return d2;
+    }
+
+    public static void setD2(Long d2) {
+        Task1.d2 = d2;
+    }
+
+
+    static private Float e2;
+    public static Float getE2() {
+        return e2;
+    }
+
+    public static void setE2(Float e2) {
+        Task1.e2 = e2;
+    }
+
+
+    static private Double f2;
+    public static Double getF2() {
+        return f2;
+    }
+
+    public static void setF2(Double f2) {
+        Task1.f2 = f2;
+    }
+
+
+
+    static private Boolean g2;
+    public static Boolean getG2() {
+        return g2;
+    }
+
+    public static void setG2(Boolean g2) {
+        Task1.g2 = g2;
+    }
+
+
+
+    static private Character h2;
+    public static Character getH2() {
+        return h2;
+    }
+
+    public static void setH2(Character h2) {
+        Task1.h2 = h2;
+    }
+
+
+
+    //рисваиваем значения примитивов оберткам и выводим на экран
     public static void main(String[] args) {
+        setA2(a1);//с помощью метода set присваиваем значение полю А2 значение примитива А1
+        System.out.println(getA2());//выводим значение поля А2
+
+
 
 
     }
+
+
 }
--- src/eu/senla/sutko/task1/varibals.java
+++ src/eu/senla/sutko/task1/varibals.java
@@ -2,39 +2,39 @@ package eu.senla.sutko.task1;
 
 public class varibals {
 
-    public   byte a;
+    private    byte a;
     public byte getA() { return a; }
     public void setA(byte a) { this.a = a; }
 
-    public  short b;
+    private  short b;
     public short getB() {return b;}
     public void setB(short b) {this.b = b;}
 
-    public    int c;
+    private    int c;
     public int getC() {return c;}
     public void setC(int c) {this.c = c;}
 
-    public    long d;
+    private    long d;
     public long getD() {return d;}
     public void setD(long d) {this.d = d;}
 
-    public    float e;
+    private    float e;
     public float getE() {return e;}
     public void setE(float e) {this.e = e;}
 
-    public    double f;
+    private    double f;
     public double getF() {return f;}
     public void setF(double f) {this.f = f;}
 
-    public    boolean g;
+    private    boolean g;
     public boolean isG() {return g;}
     public void setG(boolean g) {this.g = g;}
 
-    public    char h;
+    private    char h;
     public char getH() {return h;}
     public void setH(char h) {this.h = h;}
 
-    public    String i;
+    private    String i;
     public String getI() {return i;}
     public void setI(String i) {this.i = i;}
 
